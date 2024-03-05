168731abe90c63221dbf14c42a493cfde90bbacf
David Perez <david.perez@inspirato.com>
2024-03-04 21:03:46 -0600

    Fixingtf and node js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-const http = require(‘http’);
-const hostname = ‘localhost’;
+const http = require('http');
+const hostname = 'localhost';
 const port = 3000;
 const server = http.createServer((req, res) => {
  console.log(req.headers);
  res.statusCode = 200;
- res.end(‘<html><body><h1>Hello, World!</h1></body></html>’);
+ res.end('<html><body><h1>Hello, World!</h1></body></html>');
 })
 server.listen(port, hostname);
 
