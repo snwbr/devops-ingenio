b2b67631eed081fcb145b9c744cfe742e533c031
David Perez <david.perez@inspirato.com>
2024-03-06 12:57:39 -0600

    Adding a new IP type global

--- terraform/infra/ingenio/address.tf
+++ terraform/infra/ingenio/address.tf
@@ -4,3 +4,8 @@ resource "google_compute_address" "address" {
   address_type = "EXTERNAL"
   region       = "us-central1"
 }
+resource "google_compute_global_address" "address" {
+  project      = var.project
+  name         = "${var.env}-ingress-lb"
+  address_type = "EXTERNAL"
+}
